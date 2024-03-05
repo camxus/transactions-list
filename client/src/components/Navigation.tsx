9af8124bc3520cff5a453cc85b0aef427a4865a9
Camillus Konkwo <camillus.konkwo@gmail.com>
2024-03-05 10:58:51 +0100

    fix web3 module inject missing

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,10 +1,11 @@
 import React, { useCallback, useState } from "react";
+import injectedModule from "@web3-onboard/injected-wallets";
 import Onboard, { WalletState } from "@web3-onboard/core";
 
 import SendTransaction from "./SendTransaction";
 
 const onboard = Onboard({
-  wallets: [],
+  wallets: [injectedModule()],
   chains: [
     {
       id: "123456",
@@ -33,7 +34,7 @@ const Navigation: React.FC = () => {
 
   return (
     <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-ful text-sm py-4 bg-gray-800">
-      <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+      <nav className="max-w-[85rem] w-full mx-auto px-4 flex flex-wrap max-[320px]:block items-center justify-between">
         <div className="flex items-center justify-between">
           <a
             className="flex-none text-xl font-semibold dark:text-white"
@@ -42,26 +43,25 @@ const Navigation: React.FC = () => {
             Transactions List
           </a>
         </div>
-        <div className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
-          <div className="flex flex-col gap-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
-            {wallet && (
-              <>
-                <SendTransaction />
-                <p className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-gray-200 text-sm">
-                  {wallet.accounts[0].address}
-                </p>
-              </>
-            )}
-            {!wallet && (
-              <button
-                type="button"
-                onClick={handleConnect}
-                className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-gray-200 hover:text-white hover:bg-gray-500 hover:border-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-offset-2 transition-all text-sm"
-              >
-                Connect Wallet
-              </button>
-            )}
-          </div>
+
+        <div className="flex gap-5 justify-end sm:justify-end flex-1 flex-row sm:items-center sm:pl-5">
+          {wallet && (
+            <>
+              <SendTransaction />
+              <p className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-gray-200 text-sm">
+                {wallet.accounts[0].address}
+              </p>
+            </>
+          )}
+          {!wallet && (
+            <button
+              type="button"
+              onClick={handleConnect}
+              className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border-2 border-gray-200 font-semibold text-gray-200 hover:text-white hover:bg-gray-500 hover:border-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:ring-offset-2 transition-all text-sm"
+            >
+              Connect Wallet
+            </button>
+          )}
         </div>
       </nav>
     </header>
